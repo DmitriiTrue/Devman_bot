2142b7ee7dc0269ec7da3436afb630e33d0b0dee
Dmitrii Rokhanov <robespier1983@gmail.com>
2022-11-18 22:01:37 +0300

    Added a pause for multiple failed attempts to connect to the server

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ bot = telegram.Bot(token=os.getenv('TG_BOT_TOKEN'))
 def main():
     token = os.getenv('DEVMAN_TOKEN')
     headers = {'Authorization': token}
+    connection_attempts = 0
     while True:
         try:
             response = requests.get('https://dvmn.org/api/long_polling', headers=headers)
@@ -32,8 +33,11 @@ def main():
                                f" {lesson_info['lesson_url']}."
             bot.send_message(text=message_text, chat_id=os.getenv('MY_ID'))
         except requests.exceptions.ConnectionError:
+            connection_attempts += 1
+            if connection_attempts > 5:
+                time.sleep(60 * 60)
+                connection_attempts = 0
             print('There is no connection to the server. Try later')
-            time.sleep(60 * 10)
         except requests.exceptions.ReadTimeout:
             continue
 
