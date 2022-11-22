2312740a5ec7cb10c93288ae0a3a1f6b98667815
Dmitrii Rokhanov <robespier1983@gmail.com>
2022-11-22 18:03:12 +0300

    bagfix timestamp

--- main.py
+++ main.py
@@ -11,27 +11,33 @@ def main():
     load_dotenv()
     bot = telegram.Bot(token=os.getenv('TG_BOT_TOKEN'))
     headers = {'Authorization': os.getenv('DEVMAN_TOKEN')}
+    timestamp = time.time()
     connection_attempts = 0
     while True:
         try:
-            params = {'timestamp':time.time()}
+            params = {'timestamp':timestamp}
             response = requests.get('https://dvmn.org/api/long_polling', headers=headers,
-                                                         params=params, timeout=60)
+                                                         params=params)
             response.raise_for_status()
-            lesson_info = response.json()['new_attempts'][0]
-            if lesson_info['is_negative']:
-                message_text = dedent("""
-                Один из твоих уроков '{}' проверен.
-                Урок не принят, потому что ты косяк.
-                Жми на ссылку - {} и исправляй.
-                """.format(lesson_info['lesson_title'], lesson_info['lesson_url']))
+            print(response.json())
+            if response.json()['status']=='timeout':
+                timestamp = response.json()['timestamp_to_request']
             else:
-                message_text = dedent("""
-                Один из твоих уроков '{}' проверен.
-                Наконец-то они оценили твою гениальность.
-                Жми на ссылку - {}."""
-                .format(lesson_info['lesson_title'], lesson_info['lesson_url']))
-            bot.send_message(text=message_text, chat_id=os.getenv('RECIPIENT_ID'))
+                timestamp = response.json()['last_attempt_timestamp']
+                lesson_info = response.json()['new_attempts'][0]
+                if lesson_info['is_negative']:
+                    message_text = dedent("""
+                    Один из твоих уроков '{}' проверен.
+                    Урок не принят, потому что ты косяк.
+                    Жми на ссылку - {} и исправляй.
+                    """.format(lesson_info['lesson_title'], lesson_info['lesson_url']))
+                else:
+                    message_text = dedent("""
+                    Один из твоих уроков '{}' проверен.
+                    Наконец-то они оценили твою гениальность.
+                    Жми на ссылку - {}."""
+                    .format(lesson_info['lesson_title'], lesson_info['lesson_url']))
+                bot.send_message(text=message_text, chat_id=os.getenv('RECIPIENT_ID'))
         except requests.exceptions.ConnectionError:
             connection_attempts += 1
             if connection_attempts > 5:
